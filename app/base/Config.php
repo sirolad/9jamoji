4abed4a725eab625ba7845d04f64cecf714702de
andela-sakande <surajudeen.akande@andela.com>
2015-12-03 11:48:00 +0100

    Amend to enable test

--- app/base/Config.php
+++ app/base/Config.php
@@ -24,7 +24,7 @@ class Config
     public static function loadenv()
     {
         If (! getenv('APP_ENV') == 'production') {
-            $dotenv = new Dotenv($_SERVER['DOCUMENT_ROOT']);
+            $dotenv = new Dotenv(__DIR__.'/../..');
             $dotenv->load();
         }
     }
